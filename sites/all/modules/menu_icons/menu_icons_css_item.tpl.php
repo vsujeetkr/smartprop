b3e87f8ac894864816d89cca8530d6e3da023f8c
vsujeetkr <vsujeetkumar@gmail.com>
2018-01-19 07:54:06 +0530

    Added Modules and Libraries and themes

---
+++ sites/all/modules/menu_icons/menu_icons_css_item.tpl.php
@@ -0,0 +1,22 @@
+<?php
+global $base_url;
+/**
+ * @file
+ *
+ * Template file for generating the CSS file used for the menu-items
+ */
+
+/**
+ * Variables:
+ * $mlid
+ * $path
+ */
+?>
+a.menu-<?php print $mlid ?>, ul.links li.menu-<?php print $mlid ?> a {
+  background-image: url(<?php echo $base_url.$path ?>);
+  padding-<?php print "$pos:$size"?>px;
+  background-repeat: no-repeat;
+  background-position: <?php print $pos?>;
+  height: <?php print $height?>px;
+}
+
